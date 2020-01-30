7a2f8e3bea5767c4c6b4cbadb552b786bbd52e20
andrew <andrew@textile.io>
2020-01-29 23:11:51 -0800

    ci: make unit tests runnable in ci
    Signed-off-by: andrew <andrew@textile.io>

--- api/pb/android/threads/src/test/java/io/textile/threads/ClientUnitTest.java
+++ api/pb/android/threads/src/test/java/io/textile/threads/ClientUnitTest.java
@@ -1,13 +1,11 @@
 package io.textile.threads;
 
-//import org.junit.FixMethodOrder;
-//import org.junit.Test;
-//import org.junit.runners.MethodSorters;
 import org.json.JSONObject;
-import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
-import org.junit.jupiter.api.Order;
+import org.junit.Test;
+import org.junit.FixMethodOrder;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.junit.runners.MethodSorters;
 
 
 import io.textile.threads_grpc.GetStoreLinkReply;
@@ -15,7 +13,8 @@ import io.textile.threads_grpc.ModelCreateReply;
 
 import static org.junit.Assert.*;
 
-@TestMethodOrder(OrderAnnotation.class)
+@RunWith(JUnit4.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ClientUnitTest {
 
     static Client client;
@@ -28,45 +27,38 @@ public class ClientUnitTest {
     }
 
     @Test
-    @Order(1)
-    public void StartTest() throws Exception {
+    public void t01_StartTest() throws Exception {
         connect();
         assertEquals(true, client.connected());
     }
 
     @Test
-    @Order(2)
-    public void NewStore() throws Exception {
+    public void t02_NewStore() throws Exception {
         storeId = client.NewStoreSync();
 
         assertEquals(36, storeId.length());
     }
 
     @Test
-    @Order(3)
-    public void StartStore() throws Exception {
+    public void t03_StartStore() throws Exception {
         client.StartSync(storeId);
         assertTrue(true);
     }
 
     @Test
-    @Order(4)
-    public void GetStoreLink() throws Exception {
+    public void t04_GetStoreLink() throws Exception {
         GetStoreLinkReply reply = client.GetStoreLinkSync(storeId);
         assertNotEquals(0, reply.getAddressesCount());
     }
 
     @Test
-    @Order(4)
-    public void RegisterSchema() throws Exception {
+    public void t05_RegisterSchema() throws Exception {
         String schema = "{\\r\\n  '\\\\$id': 'https:\\/\\/example.com\\/person.schema.json',\\r\\n  '\\\\$schema': 'http:\\/\\/json-schema.org\\/draft-07\\/schema#',\\r\\n  'title': 'Person',\\r\\n  'type': 'object',\\r\\n  'required': ['ID'],\\r\\n  'properties': {\\r\\n    'ID': {\\r\\n      'type': 'string',\\r\\n      'description': 'The entity\\\\'s id.',\\r\\n    },\\r\\n    'firstName': {\\r\\n      'type': 'string',\\r\\n      'description': 'The person\\\\'s first name.',\\r\\n    },\\r\\n    'lastName': {\\r\\n      'type': 'string',\\r\\n      'description': 'The person\\\\'s last name.',\\r\\n    },\\r\\n    'age': {\\r\\n      'description': 'Age in years which must be equal to or greater than zero.',\\r\\n      'type': 'integer',\\r\\n      'minimum': 0,\\r\\n    },\\r\\n  },\\r\\n};";
         client.RegisterSchemaSync(storeId, "Person", schema);
         assertTrue(true);
     }
 
-    @Test
-    @Order(5)
-    public void ModelCreate() throws Exception {
+    public void t06_ModelCreate() throws Exception {
         JSONObject person = createPerson("ABCDEF", 22);
         String[] data = { person.toString() };
         ModelCreateReply reply = client.ModelCreateSync(storeId, "Person", data);
