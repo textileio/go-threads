cca9fab69496927f64a89757a095d2a290463a39
Aaron Sutula <hi@asutula.com>
2020-04-06 19:13:38 -0500

    ulid for instance id (#304)
    * use ulid for instance id, clean up id verification
    
    Signed-off-by: Aaron Sutula <hi@asutula.com>
    
    * use crypto/rand for id gen
    
    Signed-off-by: Aaron Sutula <hi@asutula.com>
    
    * revert error handling
    
    Signed-off-by: Aaron Sutula <hi@asutula.com>
    
    * try removing -race
    
    Signed-off-by: Aaron Sutula <hi@asutula.com>
    
    * put -race back
    
    Signed-off-by: Aaron Sutula <hi@asutula.com>
    
    * try uuid again
    
    Signed-off-by: Aaron Sutula <hi@asutula.com>
    
    * Revert "try uuid again"
    
    This reverts commit f3a8c66b67d9d3e0e2eb57fbe7fb19164d8af2a1.
    
    Signed-off-by: Aaron Sutula <hi@asutula.com>
    
    * tone down foldersync tests
    
    Signed-off-by: Aaron Sutula <hi@asutula.com>
    
    * Fix android test
    
    Signed-off-by: Aaron Sutula <hi@asutula.com>

--- core/db/db.go
+++ core/db/db.go
@@ -1,9 +1,11 @@
 package db
 
 import (
-	"github.com/google/uuid"
+	"crypto/rand"
+
 	ds "github.com/ipfs/go-datastore"
 	format "github.com/ipfs/go-ipld-format"
+	"github.com/oklog/ulid/v2"
 )
 
 const (
@@ -16,18 +18,17 @@ type InstanceID string
 
 // NewInstanceID generates a new identity for an instance.
 func NewInstanceID() InstanceID {
-	return InstanceID(uuid.New().String())
+	id := ulid.MustNew(ulid.Now(), rand.Reader)
+	return InstanceID(id.String())
 }
 
 func (e InstanceID) String() string {
 	return string(e)
 }
 
+// IsValidInstanceID checks if an id is valid
 func IsValidInstanceID(instanceID string) bool {
-	// ToDo: Decide how we want to apply instance id requirements
 	return len(instanceID) > 0
-	// _, err := uuid.Parse(instanceID)
-	// return err == nil
 }
 
 // Event is a local or remote event generated in collection and dispatcher
