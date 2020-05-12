3cd5138bc6a3d10d604fd7b117169ce1afbdb803
Aaron Sutula <hi@asutula.com>
2020-05-12 09:49:11 -0500

    Revert "use Undef to test"
    This reverts commit 76bb790aa8d8bf57c089faf7de1402ff81a3ce27.
    
    Signed-off-by: Aaron Sutula <hi@asutula.com>

--- core/thread/id.go
+++ core/thread/id.go
@@ -86,7 +86,7 @@ var Undef = ID("")
 // Calling any other methods on an undefined ID will result in
 // undefined behavior.
 func (i ID) Defined() bool {
-	return i != Undef
+	return i != ""
 }
 
 // Decode parses an ID-encoded string and returns an ID object.
