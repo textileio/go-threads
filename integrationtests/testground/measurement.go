3ae8a7df34f6ebc9673153849c6c313329dc1fb9
Merlin Ran <merlinran@gmail.com>
2021-06-11 11:11:16 -0400

    switch to collect goroutines and heap alloc
    Node exporter CPU and memory are at the host level, not within a container
    We can switch to cAdvisor to collect cgroup resources, but too overkill.
    Because Go doesn't release unused memory, memory usage from outside tend
    to be inaccurate.
    
    Signed-off-by: Merlin Ran <merlinran@gmail.com>

--- integrationtests/testground/measurement.go
+++ integrationtests/testground/measurement.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	goruntime "runtime"
 	"time"
 
 	"github.com/go-kit/kit/log"
@@ -14,45 +15,37 @@ import (
 )
 
 type measure struct {
-	runenv         *runtime.RunEnv
-	registry       *prometheus.Registry
-	chStop         chan struct{}
-	metrics        map[string][]float64
-	lastCPUSeconds float64
-	lastRecv       float64
-	lastTransmit   float64
+	runenv       *runtime.RunEnv
+	registry     *prometheus.Registry
+	chStop       chan struct{}
+	metrics      map[string][]float64
+	lastRecv     float64
+	lastTransmit float64
 }
 
 const (
-	metricCPU           = "cpu-seconds"
-	metricMemory        = "active-memory-mibs"
+	metricGoroutines    = "goroutines"
+	metricHeapAllocMiBs = "heap-alloc-mibs"
 	metricRecvBytes     = "receive-bytes"
 	metricTransmitBytes = "transmit-bytes"
 )
 
-// startMeasure starts collecting CPU, active memory and transmit/receive bytes every second, until stopAndPrint is called on the returned measure, at which point it sends all the recorded metrics as test result to InfluxDB, and prints them as line graphs for inspection.
+// startMeasure starts collecting number of goroutines, golang heap allocation and transmit/receive bytes every second, until stopAndPrint is called on the returned measure, at which point it sends all the recorded metrics as test result to InfluxDB, and prints them as line graphs for inspection.
 func startMeasure(runenv *runtime.RunEnv) (*measure, error) {
 	// have to do this because node_exporter requires it being called to properly initialize global variables.
 	kingpin.Parse()
 	logger := log.NewNopLogger()
-	creators := map[string]func(log.Logger) (collector.Collector, error){
-		"cpu":    collector.NewCPUCollector,
-		"memory": collector.NewMeminfoCollector,
-		"net":    collector.NewNetDevCollector,
-	}
 	registry := prometheus.NewRegistry()
 	collector.DisableDefaultCollectors()
 	nodeCollector, err := collector.NewNodeCollector(logger)
 	if err != nil {
 		return nil, err
 	}
-	for name, f := range creators {
-		c, err := f(logger)
-		if err != nil {
-			return nil, err
-		}
-		nodeCollector.Collectors[name] = c
+	netCollector, err := collector.NewNetDevCollector(logger)
+	if err != nil {
+		return nil, err
 	}
+	nodeCollector.Collectors["net"] = netCollector
 	registry.MustRegister(nodeCollector)
 	p := &measure{runenv: runenv, registry: registry,
 		chStop:  make(chan struct{}),
@@ -76,49 +69,32 @@ func (p *measure) Collect() {
 			}
 			for _, m := range mf {
 				switch *m.Name {
-				case "node_cpu_seconds_total":
-					p.calcCPU(m.Metric)
 				case "node_network_receive_bytes_total":
-					p.calcRecv(m.Metric)
+					p.collectRecv(m.Metric)
 				case "node_network_transmit_bytes_total":
-					p.calcTransmit(m.Metric)
-				case "node_memory_active_bytes":
-					p.calcMemory(m.Metric)
+					p.collectTransmit(m.Metric)
 				}
 			}
+			p.collectGoroutines()
+			p.collectMemStats()
 		case <-p.chStop:
 			return
 		}
 	}
 }
 
-func (p *measure) calcCPU(metrics []*dto.Metric) {
-	var total, exclude float64
-	for _, m := range metrics {
-		total += *m.Counter.Value
-		for _, label := range m.Label {
-			if *label.Name == "mode" && (*label.Value == "idle" || *label.Value == "steal") {
-				exclude += *m.Counter.Value
-			}
-		}
-	}
-	total = total - exclude
-	usage := total - p.lastCPUSeconds
-	if p.lastCPUSeconds > 0 {
-		p.metrics[metricCPU] = append(p.metrics[metricCPU], usage)
-		p.runenv.D().Gauge(metricCPU).Update(usage)
-	}
-	p.lastCPUSeconds = total
+func (p *measure) collectGoroutines() {
+	p.metrics[metricGoroutines] = append(p.metrics[metricGoroutines], float64(goruntime.NumGoroutine()))
 }
 
-func (p *measure) calcMemory(metrics []*dto.Metric) {
-	mem := *metrics[0].Gauge.Value
-	p.metrics[metricMemory] = append(p.metrics[metricMemory], mem/1048576.0)
-	p.runenv.D().Gauge(metricMemory).Update(mem)
+func (p *measure) collectMemStats() {
+	var m goruntime.MemStats
+	goruntime.ReadMemStats(&m)
+	p.metrics[metricHeapAllocMiBs] = append(p.metrics[metricHeapAllocMiBs], float64(m.HeapAlloc)/1048576.0)
 }
 
-func (p *measure) calcRecv(metrics []*dto.Metric) {
-	total := p.calcBytes(metrics)
+func (p *measure) collectRecv(metrics []*dto.Metric) {
+	total := p.collectBytes(metrics)
 	usage := total - p.lastRecv
 	if p.lastRecv > 0 {
 		p.metrics[metricRecvBytes] = append(p.metrics[metricRecvBytes], usage)
@@ -127,8 +103,8 @@ func (p *measure) calcRecv(metrics []*dto.Metric) {
 	p.lastRecv = total
 }
 
-func (p *measure) calcTransmit(metrics []*dto.Metric) {
-	total := p.calcBytes(metrics)
+func (p *measure) collectTransmit(metrics []*dto.Metric) {
+	total := p.collectBytes(metrics)
 	usage := total - p.lastTransmit
 	if p.lastTransmit > 0 {
 		p.metrics[metricTransmitBytes] = append(p.metrics[metricTransmitBytes], usage)
@@ -137,7 +113,7 @@ func (p *measure) calcTransmit(metrics []*dto.Metric) {
 	p.lastTransmit = total
 }
 
-func (p *measure) calcBytes(metrics []*dto.Metric) float64 {
+func (p *measure) collectBytes(metrics []*dto.Metric) float64 {
 	var total, exclude float64
 	for _, m := range metrics {
 		total += *m.Counter.Value
@@ -154,8 +130,8 @@ func (p *measure) stopAndPrint() {
 	close(p.chStop)
 	output := fmt.Sprintf("Test params: %v", p.runenv.TestInstanceParams)
 	for _, name := range []string{
-		metricCPU,
-		metricMemory,
+		metricGoroutines,
+		metricHeapAllocMiBs,
 		metricRecvBytes,
 		metricTransmitBytes,
 	} {
